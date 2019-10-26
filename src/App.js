72c9558bbbd3c650569402ec9262627e9ba27f41
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-10-26 23:00:24 +0300

    Setup deploy to Surge.sh via Travis CI

--- src/App.js
+++ src/App.js
@@ -5,30 +5,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-        <Button variant="contained">
-          {process.env.NODE_ENV}
-        </Button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
+                <Button variant="contained">
+                    {process.env.NODE_ENV}
+                </Button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
